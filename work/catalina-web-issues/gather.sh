eb4056f2fdc063808970ba6c547ad88e0f50899e
Ondrej Kolacek <ondrej.kolacek@avast.com>
2020-01-10 13:55:28 +0100

    more data

--- work/catalina-web-issues/gather.sh
+++ work/catalina-web-issues/gather.sh
@@ -5,7 +5,11 @@ eval GATHER="~/Documents/avast_gather"
 rm -rf "$GATHER"
 mkdir -p "$GATHER"
 
-#sample avast processes
+exec &> >(tee -a "$GATHER/gather.log")
+
+
+
+echo "sampling avast processes"
 sudo /usr/bin/sample com.avast.proxy -f "$GATHER/avast_proxy.sample"
 sudo /usr/bin/sample com.avast.fileshield -f "$GATHER/avast_fileshield.sample"
 sudo /usr/bin/sample com.avast.service -f "$GATHER/avast_service.sample"
@@ -18,7 +22,12 @@ for i in $(ps -e|grep Firefox|grep "Contents/MacOS/plugin-container.app/Contents
 for i in $(ps -e|grep "Contents/MacOS/Safari"|grep -v " grep "|awk '{print $1}'); do echo "sampling safari $i:"; sudo /usr/bin/sample $i 3 -f "$GATHER/safari$i.sample";  done
 for i in $(ps -e|grep "Contents/MacOS/com.apple.WebKit"|grep -v " grep "|awk '{print $1}'); do echo "sampling webkit $i:"; sudo /usr/bin/sample $i 3 -f "$GATHER/webkit$i.sample";  done
 
+#sample Finder
+for i in $(ps -e|grep "Contents/MacOS/Finder"|grep -v " grep "|awk '{print $1}'); do echo "sampling Finder $i:"; sudo /usr/bin/sample $i 3 -f "$GATHER/finder$i.sample";  done
+
 #generate support package
 SUPPORT_PKG="$(/Applications/Avast.app/Contents/Backend/scripts/com.avast.supportpkg)"
-if [ -f "$SUPPORT_PKG" ]; then cp "$SUPPORT_PKG" "$GATHER"; fi
+if [ -f "$SUPPORT_PKG" ]; then mv "$SUPPORT_PKG" "$GATHER/support_from_gather.zip"; fi
 
+
+echo "script gather.sh has finished gathering data"
